51b540a7346925bafdb19578366610931853fed6
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-24 14:41:22 -0500

    Se define la libreria asi como las primeras variables que utilizaremos en el programa ademas de ingresar la parte donde el usuario ingresa el numero que se usara en la funcion

---
+++ PruebaPROGRESO1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//Kevin Johan Maquis Calle
+
+int main(){
+
+    int numingresa,cont1,cont2;
+
+    printf("\nIngrese el numero qe quiere convertir en binario");
+    scanf("%d",&numingresa);
+
+    if (numingresa<0)
+    {
+        numingresa=-numingresa;
+    }
+
+
+
+
+
+}
