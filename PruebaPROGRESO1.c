dd599ed3ad207c3f25533875ee5a21018faa03a4
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-24 15:11:30 -0500

    Se puso la funcion y se agrego ademas otra funcion repetitiva

--- PruebaPROGRESO1.c
+++ PruebaPROGRESO1.c
@@ -3,15 +3,36 @@
 
 int main(){
 
-    int numingresa,cont1,cont2;
+    int numingresa,cont2,numbinario;
 
-    printf("\nIngrese el numero qe quiere convertir en binario");
+    printf("\nIngrese el numero qe quiere convertir en binario: ");
     scanf("%d",&numingresa);
 
     if (numingresa<0)
     {
         numingresa=-numingresa;
     }
+    printf("El numero binario es");
+    int cont1=numingresa/2;
+    while (cont1!=0)
+    {
+        cont2=numingresa%2;
+        numingresa/=2;
+
+        if (cont2==0)
+        {
+            numbinario=1;
+            printf("%d",numbinario);
+        }
+        else
+        {
+            numbinario=0;
+            printf("%d",numbinario);
+        }
+        
+        cont1--;
+    }
+    
 
 
 
